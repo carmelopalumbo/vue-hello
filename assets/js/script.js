f2677b24e587fcec4eed17d56c7a9fc2ff5db435
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-02 18:37:22 +0100

    fix-p

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,8 @@ createApp({
             message: 'Benvenuto Vue!',
             myClass: 'title_class',
             image: '/assets/img/vuejs-wide.png',
-            logo: 'logo_style'
+            logo: 'logo_style',
+            info: 'Vue.js è un framework JavaScript open-source in configurazione Model view viewmodel per la creazione di interfacce utente e single-page applications. È stato creato da Evan You ed è gestito da lui stesso e i membri attivi del core team, provenienti da varie società come Netlify e Netguru.'
         }
     },
 
