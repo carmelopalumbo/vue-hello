92509181a84138d11bea6734917c35ca6ab2cfca
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-02 13:41:50 +0100

    add-structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 // vue
+
+//inizializzo l'app
+const { createApp } = Vue;
+
+createApp({
+
+    //contenuto dell'app
+    data(){
+        return{
+
+        }
+    },
+
+    //metodi dell'app
+    methods: {
+
+    }
+}).mount('#app')
